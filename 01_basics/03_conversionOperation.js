f34d97ac91defe02dd9135a7cc66face60a59d1b
pranav-jss <159114564+pranav-jss@users.noreply.github.com>
2024-02-07 08:52:07 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,19 +34,70 @@ console.log(valueInNumbers);
 //true=1
 //false=0
 
+/*
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(typeof booleanIsLoggedIn);
 console.log(booleanIsLoggedIn);
+*/
 
 //1 = true
 //0 = false
 //""= false
 //"pranav"= true
 
+/*
 let someNumber= 55;
 
 let stringNumber= String(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+let value= 3;
+let negValue = -value;
+console.log(negValue);
+console.log(typeof negValue);
+*/
+
+
+//..........................................Operations.........................................
+
+/*
+console.log (2+2);
+console.log(2*2);
+console.log (2/2);
+console.log(2%2);
+console.log(2/3);
+console.log(2%3);
+console.log(2**3);
+*/
+
+/*
+let str1= "hello";
+let str2= " pranav";
+let str3 = str1+str2;
+console.log(str3);
+*/
+
+/*
+console.log(1+2+3);         // 6
+console.log("1" + 2);       // 12
+console.log(1 + "2");       // 12
+console.log("1" +2 +2);     //122
+console.log(1 + 2 + "2");   //32
+
+console.log(true);   //true
+console.log(+true);  //1
+console.log(true+); //error
+console.log(+ "");  //0
+*/
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+//link to study
+//https://tc39.es/ecma262/#sec-abstract-operations
+// mdn prefix and postfix read
