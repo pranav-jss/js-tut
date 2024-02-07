f410748ea5d4dedaa7c5d5bcf3cc51d52accd908
pranav-jss <159114564+pranav-jss@users.noreply.github.com>
2024-02-07 07:44:05 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+/*let score =33;
+
+console.log(typeof score);
+console.log (typeof (score));
+
+let scoree ="33abc";
+
+console.log(typeof scoree);
+console.log(typeof (scoree));
+*/
+
+/*
+let age = "33abc";
+
+console.log(typeof age);
+
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+/*
+let level = true;
+
+console.log(typeof level);
+
+let valueInNumbers = Number(level);
+console.log(typeof valueInNumbers);
+console.log(valueInNumbers);
+*/
+
+//null = 0
+//undefined = NaN
+//true=1
+//false=0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 = true
+//0 = false
+//""= false
+//"pranav"= true
+
+let someNumber= 55;
+
+let stringNumber= String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
