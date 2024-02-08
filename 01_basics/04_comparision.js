e8ff1bbc64361cee637580d69d38eb288fa6fb18
pranav-jss <159114564+pranav-jss@users.noreply.github.com>
2024-02-08 08:13:23 +0000

    comparision values and their datatypes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -9,19 +9,19 @@ console.log(2<=1);
 console.log(2==1);
 */
 
-console.log(null>0);
-console.log(null==0);
-console.log(null>=0);
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
 
 // Here, equality check == and comparision < > >= <= works differently. 
 //In comparision the null is converted into a number and is treated as 0.
 //while in equality check the null is considered as null and not a number.
 
-console.log(undefined == 0);
-console.log(undefined >=0);
-console.log(undefined >0);
-console.log(undefined <0);
+console.log(undefined == 0); //false
+console.log(undefined >=0); //false
+console.log(undefined >0);  //false
+console.log(undefined <0);  //false
 
 
-console.log("2"==2);
-console.log("2"===2); //strict check. it also checks its datatypes
+console.log("2"==2); // True: bcoz it doesnot check datatypes
+console.log("2"===2); //false. Strict check. it also checks its datatypes
