fab565914bcaf5e0bd3cdbd69cd4010000e04970
pranav-jss <159114564+pranav-jss@users.noreply.github.com>
2024-02-10 04:13:00 +0000

    datatypes summary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -23,4 +23,44 @@ A. Javascript is dynamically typed language which means datatypes of variables a
 
     */
 
+    //array ex:
+
+    /*   const heros ["hulk", "loki", "thor" ];   */
+
+    //object ex:
+
+      let myObj={
+        name ="pranav",
+        age="32",
+            }  
+
+    // function ex:
+
+    const myFunction = function () {
+
+        console.log("hello world");
+
+    }
+
+
+    // to determine the datatype we use typeof function :
+
+    console.log(BigInt);
+
+    console.log(typeof myFunction);
+    console.log(typeof myObj);
     
+
+    //determine the typeof of all the datatypes or else refer ecma script documentation 
+
+
+
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    Memory has two types:
+    1. Stack Memory (Primitive datapyes me har jagah stack memory use hoti hai): jab bhi stack 
+        memory use hoti hai iska matlab jo bhi apne variable declare kara hai uska apko copy milta hai
+    
+    2. Heap memory (Non-primitve me har jagah heap memory use hoti hai): jab bhi heap memory allocate
+        hoti hai to apko reference milta hai original value ka. to jo bhi changeshonge wo sidha original
+        value me hota hai
